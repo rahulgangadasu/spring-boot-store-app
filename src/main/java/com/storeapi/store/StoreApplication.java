f29a4654b09b6c8dd77b695ca28e63e05ef2a1d0
rahulgangadasu <rahulreddygangadasu2709@gmail.com>
2026-05-13 17:26:17 -0500

    Add starter files

--- src/main/java/com/storeapi/store/StoreApplication.java
+++ src/main/java/com/storeapi/store/StoreApplication.java
@@ -8,6 +8,7 @@ public class StoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StoreApplication.class, args);
+        System.out.println("Spring Boot Application Started");
     }
 
 }
